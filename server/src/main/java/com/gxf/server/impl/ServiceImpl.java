6ede3d216a59fbb54e790438de2e0b18e8572da9
luckygxf <403261934@qq.com>
2018-01-08 14:12:12 +0800

    finish demo | add log4j2 | next analyze demo

--- server/src/main/java/com/gxf/server/impl/ServiceImpl.java
+++ server/src/main/java/com/gxf/server/impl/ServiceImpl.java
@@ -2,14 +2,17 @@ package com.gxf.server.impl;
 
 import com.gxf.model.User;
 import com.gxf.service.IService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Created by 58 on 2018/1/8.
  */
 public class ServiceImpl implements IService {
-
+    private static Logger logger = LogManager.getLogger(ServiceImpl.class);
 
     public User getUser() {
+        logger.info("Receive reqeust");
         System.out.println("Recevie request");
         return new User("guanxiangseneg", 28);
     }
