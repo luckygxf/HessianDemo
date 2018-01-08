663e40ad1960445a7a502ce51cc899b5c0bf9aaf
luckygxf <403261934@qq.com>
2018-01-08 08:37:46 +0800

    add common | next dev server and client

---
+++ common/src/main/java/com/gxf/service/IService.java
@@ -0,0 +1,15 @@
+package com.gxf.service;
+
+import com.gxf.model.User;
+
+/**
+ * Created by GuanXF on 2018/1/8.
+ * 对外提供的接口
+ */
+public interface IService {
+
+    /**
+     * 获取用户
+     * */
+    public User getUser();
+}
